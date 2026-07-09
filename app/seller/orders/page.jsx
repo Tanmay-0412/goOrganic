09242af5b394453712efd01d68fa51fe7d44ffac
Tanmay <tanmay@codesis.tech>
2026-07-09 19:57:14 +0530

    repaired AppContext

--- app/seller/orders/page.jsx
+++ app/seller/orders/page.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
-import { useAppContext } from "@/context/AppContext";
+import {useAppContext} from "@/context/AppContext"
 import Footer from "@/components/seller/Footer";
 import Loading from "@/components/Loading";
 
